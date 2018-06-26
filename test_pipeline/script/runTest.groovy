826f452abc796fceb581d311e1a01abf48b3add3
Tran Truong <tran.truong@dektech.com.au>
2018-06-26 10:45:43 +0700

    Add runTest.groovy for testing local jenkins

---
+++ test_pipeline/script/runTest.groovy
@@ -0,0 +1,13 @@
+node {
+    stage('Stage 1') {
+        echo 'Hello World: ${env.AR}'
+        echo "env.ARTIFACT_VERSIO : ${env.ARTIFACT_VERSION}"
+        echo "env.MULTIPLE_IMAGES: ${env.MULTIPLE_IMAGES}"
+        env1 = env.ARTIFACT_VERSION
+        env2 = env.MULTIPLE_IMAGES
+        withEnv(["env1=$env1",
+        "env2=$env2"]) {
+            sh "echo env1: ${env1}, env2: ${env2} >> /tmp/log"
+        }
+    }
+}
