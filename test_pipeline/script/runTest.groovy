ad55db1529d939b86ea1370358b23642a2de0dc2
Tran Truong <tran.truong@dektech.com.au>
2018-07-02 17:26:38 +0700

    Test function with param

--- test_pipeline/script/runTest.groovy
+++ test_pipeline/script/runTest.groovy
@@ -5,22 +5,13 @@ def funcA(option) {
 }
 node {
     a = "$HOME/a"
+    b = "$HOME/b"
     parallel singleImage: {
         stage('Stage 1') {
-	        echo 'Hello World: ${env.AR}'
-	        echo "env.ARTIFACT_VERSIO : ${env.ARTIFACT_VERSION}"
-	        echo "env.MULTIPLE_IMAGES: ${env.MULTIPLE_IMAGES}"
-	        env1 = env.ARTIFACT_VERSION
-	        env2 = env.MULTIPLE_IMAGES
-	        withEnv(["env1=$env.ARTIFACT_VERSION",
-	        "env2=$env.MULTIPLE_IMAGES",
-                "A=$a"]) {
-	            sh "echo env1: ${env1}, env2: ${env2}, a: ${A} >> /tmp/log"
-	        }
+                funcA(b)
 	    }
         }, multipleImage: {
 	    stage('Stage 2') {
-		echo "Stage 2"
                 funcA(a)
 	    }
         }
