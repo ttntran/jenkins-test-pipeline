aeeec2732818097fd1dfde02a34ecd33317146a2
Tran TN Truong <ttntran1988@yahoo.com>
2019-01-21 13:42:17 +0700

    [ci BUILD]

--- test_pipeline/script/runTest.groovy
+++ test_pipeline/script/runTest.groovy
@@ -8,7 +8,7 @@ node {
 	    }
         }, multipleImage: {
 	    stage('Stage 2') {
-                funcA("Hello!!!!!!!@@@##???&&&12abfakdjfk")
+                funcA("Hello!!!!!!!@@@##???&&&12abfakdjfkgdfjsahg")
 	    }
         }
  }
