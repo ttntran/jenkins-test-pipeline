116949bf71501a263d185d2dc7ce692184e7f434
Tran TN Truong <ttntran1988@yahoo.com>
2019-01-21 14:49:38 +0700

    build auto

--- test_pipeline/script/runTest.groovy
+++ test_pipeline/script/runTest.groovy
@@ -8,7 +8,7 @@ node {
 	    }
         }, multipleImage: {
 	    stage('Stage 2') {
-                funcA("Hello!!!!!!!@@@##???&&&12abfakdjfkgdfjsahgdafkdhellofdskjfak")
+                funcA("Hello!!!!!!!@@@##???&&&12abfakdjfkgdfjsahgdafkdhellofdskjfakadhfsdhf")
 	    }
         }
  }
